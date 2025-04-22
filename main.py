6f20d36665381532c8af2f28cba3d990b471b309
dolsey3 <deborahlfrias@gmail.com>
2025-04-22 17:04:25 -0400

    add moby dick and pride and prejudice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 if len(sys.argv) != 2:
-    print("Usage: python main.py <path_to_book>")
+    print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 book_path = sys.argv[1]
     
